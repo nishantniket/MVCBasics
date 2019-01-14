d8edebab7528b4d12b91d671f07650af0f961ece
unknown <nishantniket@gmail.com>
2019-01-14 23:28:50 +0530

    Updated Movies Controller. Added Index views for existing controller

--- MovieApp/Controllers/MoviesController.cs
+++ MovieApp/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Web;
 using System.Web.Mvc;
 using MovieApp.Models;
@@ -10,45 +11,77 @@ namespace MovieApp.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id = 1, Name = "Shrek"},
+                new Movie{Id = 2,Name = "Wall-e"}
+            };
+        }
+
+        //GET Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Test" };
+            var movie = new Movie() {Name = "Shrek!"};
             var customers = new List<Customer>
             {
-                new Customer { Name = "Customer 1" },
-                new Customer {Name = "Customer 2"}
+                new Customer{Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
             };
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
             };
-
             return View(viewModel);
         }
-         public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-            if(!String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
+        //// GET: Movies
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Test" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer {Name = "Customer 2"}
+        //    };
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
 
-            return Content($"PageIndex = {pageIndex} & sortBy = {sortBy}");
-        }
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content($"year:{year} & month: {month}");
-        }
+        //    return View(viewModel);
+        //}
+        // public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+        //    if(!String.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
+
+        //    return Content($"PageIndex = {pageIndex} & sortBy = {sortBy}");
+        //}
+        //[Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content($"year:{year} & month: {month}");
+        //}
     }
 }
